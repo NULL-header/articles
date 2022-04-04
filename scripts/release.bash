8b5b0a1ae10ea097a81662ab760b9932f022b01d
NULL-header <poaoq.poaoq@gmail.com>
2022-04-04 13:43:28 +0900

    update for zenn #16

--- scripts/release.bash
+++
@@ -1,8 +0,0 @@
-dir_script=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pushd ${dir_script%/*} > /dev/null
-
-cp ./md/*.md .
-
-shopt -s extglob
-rm -r !(!(README).md|.git|.|..)
-popd > /dev/null
